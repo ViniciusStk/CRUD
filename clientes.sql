44be72b22c16587f0c12865f976ddb6291c307f3
ViniciusStk <42050203+ViniciusStk@users.noreply.github.com>
2022-03-13 02:30:29 -0300

    Add files via upload

---
+++ clientes.sql
@@ -0,0 +1,113 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 13-Mar-2022 às 05:59
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `clientes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `contato`
+--
+
+CREATE TABLE `contato` (
+  `id` int(11) NOT NULL,
+  `tipo` varchar(8) NOT NULL,
+  `descricao` varchar(225) DEFAULT NULL,
+  `idPessoa` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `contato`
+--
+
+INSERT INTO `contato` (`id`, `tipo`, `descricao`, `idPessoa`) VALUES
+(16, 'TEL', '4551515', 4),
+(17, 'EMAIL', 'vini@vini.com.br', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pessoas`
+--
+
+CREATE TABLE `pessoas` (
+  `id` int(11) NOT NULL,
+  `name` varchar(225) NOT NULL,
+  `cpf` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `pessoas`
+--
+
+INSERT INTO `pessoas` (`id`, `name`, `cpf`) VALUES
+(3, 'enzo', '112333'),
+(4, 'kjjkjkkj', '554326523'),
+(5, 'Leonir', '106742759'),
+(7, 'oioi', '222222');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `contato`
+--
+ALTER TABLE `contato`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_contato_pessoas` (`idPessoa`);
+
+--
+-- Índices para tabela `pessoas`
+--
+ALTER TABLE `pessoas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `contato`
+--
+ALTER TABLE `contato`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- AUTO_INCREMENT de tabela `pessoas`
+--
+ALTER TABLE `pessoas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `contato`
+--
+ALTER TABLE `contato`
+  ADD CONSTRAINT `fk_contato_pessoas` FOREIGN KEY (`idPessoa`) REFERENCES `pessoas` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
